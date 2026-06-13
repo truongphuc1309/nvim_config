d42f8a6447edd468f11b007f2e7d165c880d932b
Truong Phuc <truongngo1309@gmail.com>
2026-06-13 19:31:27 +0700

    feat: Add vim-fugitvie plugin

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,10 +1,22 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		-- setup gitsigns with default properties
-		require("gitsigns").setup({})
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			-- setup gitsigns with default properties
+			require("gitsigns").setup({})
 
-		-- Set a vim motion to <Space> + g + h to preview changes to the file under the cursor in normal mode
-		vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
-	end,
+			-- Set a vim motion to <Space> + g + h to preview changes to the file under the cursor in normal mode
+			vim.keymap.set("n", "<leader>gh", ":Gitsigns preview_hunk<CR>", { desc = "[G]it Preview [H]unk" })
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = { "Git", "G" },
+		keys = {
+			{ "<leader>gg", "<cmd>Git<CR>", desc = "[G]it Status" },
+			{ "<leader>gb", "<cmd>Git blame<CR>", desc = "[G]it [B]lame" },
+			{ "<leader>gd", "<cmd>Gdiffsplit<CR>", desc = "[G]it [D]iff Split" },
+			{ "<leader>gc", "<cmd>Git commit<CR>", desc = "[G]it [C]ommit" },
+		},
+	},
 }
