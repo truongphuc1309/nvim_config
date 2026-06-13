436f21e813974c7a32353aab8918544342b65241
Truong Phuc <truongngo1309@gmail.com>
2026-06-14 01:59:37 +0700

    fix: fix bug in nvim_lint with eslint_d

--- lua/plugins/nvim_lint.lua
+++ lua/plugins/nvim_lint.lua
@@ -1,29 +1,42 @@
 return {
-  "mfussenegger/nvim-lint",
-  event = { "BufReadPre", "BufNewFile" },
-  config = function()
-    local lint = require("lint")
+	"mfussenegger/nvim-lint",
+	event = { "BufReadPre", "BufNewFile" },
+	config = function()
+		local lint = require("lint")
 
-    lint.linters_by_ft = {
-      javascript = { "eslint_d" },
-      typescript = { "eslint_d" },
-      javascriptreact = { "eslint_d" },
-      typescriptreact = { "eslint_d" },
-      svelte = { "eslint_d" },
-      python = { "pylint" },
-    }
+		local eslint_d = lint.linters.eslint_d
+		local eslint_d_parser = eslint_d.parser
 
-    local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+		eslint_d.parser = function(output, bufnr)
+			local json_start = output:find("[%[{]")
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
-      group = lint_augroup,
-      callback = function()
-        lint.try_lint()
-      end,
-    })
+			if json_start and json_start > 1 then
+				output = output:sub(json_start)
+			end
 
-    vim.keymap.set("n", "<leader>ll", function()
-      lint.try_lint()
-    end, { desc = "Trigger linting for current file" })
-  end,
+			return eslint_d_parser(output, bufnr)
+		end
+
+		lint.linters_by_ft = {
+			javascript = { "eslint_d" },
+			typescript = { "eslint_d" },
+			javascriptreact = { "eslint_d" },
+			typescriptreact = { "eslint_d" },
+			svelte = { "eslint_d" },
+			python = { "pylint" },
+		}
+
+		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
+
+		vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+			group = lint_augroup,
+			callback = function()
+				lint.try_lint()
+			end,
+		})
+
+		vim.keymap.set("n", "<leader>ll", function()
+			lint.try_lint()
+		end, { desc = "Trigger linting for current file" })
+	end,
 }
